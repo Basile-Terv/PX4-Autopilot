99de89b831bc6f67f24cb8346a78741c776f8b18
Basile-Terv <bisilou@gmail.com>
2024-01-31 11:37:32 +0100

    new msg file for IR grove devices

--- src/modules/ir_laser_game/ir_laser_game.h
+++ src/modules/ir_laser_game/ir_laser_game.h
@@ -4,9 +4,7 @@
 #include <px4_platform_common/module.h>
 #include <px4_platform_common/module_params.h>
 #include <uORB/Subscription.hpp>
-#include <uORB/topics/pwm_input.h>
-#include <uORB/topics/pwm_output.h>
-#include <uORB/topics/manual_control_setpoint.h>
+#include <uORB/topics/infrared_control.h>  // Include the new message header
 
 class IRLaserGameModule : public ModuleBase<IRLaserGameModule>, public ModuleParams
 {
@@ -30,17 +28,13 @@ public:
 private:
     void parameters_update(bool force = false);
 
-    uORB::Subscription<pwm_input_s> _pwm_input_sub{ORB_ID(pwm_input)};
-    uORB::Subscription<pwm_output_s> _pwm_output_sub{ORB_ID(pwm_output)};
-    uORB::Subscription<manual_control_setpoint_s> _manual_control_setpoint_sub{ORB_ID(manual_control_setpoint)};
+    uORB::Subscription<infrared_control_s> _infrared_control_sub{ORB_ID(infrared_control)};  // Change the subscription to the new message
 
     // Add your member variables here
     bool _ir_signal_detected{false};
     hrt_abstime _ir_signal_timestamp{0};
 
     // Add your helper methods here
-    void handle_pwm_inputs(const pwm_input_s &pwm_input);
-    void handle_manual_control_setpoint(const manual_control_setpoint_s &manual_control_setpoint);
+    void handle_infrared_control(const infrared_control_s &infrared_control);
     void send_ir_signal();
 };
-
